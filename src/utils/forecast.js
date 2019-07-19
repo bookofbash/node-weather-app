f27c31e59aa8260134c282c4bb5fb5daa95365d2
Bashir <harrell.bashir@yahoo.com>
2019-07-19 11:56:28 -0400

    Added daily high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,8 +7,7 @@ const forcast = (longitude,latitude, callback)=>{
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback( undefined, body.daily.data[0].summary + " It is currently "+ body.currently.temperature + " degrees. There is a "+ body.currently.precipProbability +'%'+ ' chance of rain' 
-  
+            callback( undefined, body.daily.data[0].summary + " It is currently "+ body.currently.temperature + " degrees. There is a "+ body.currently.precipProbability +'%'+ ' chance of rain. '+'The high today is '+ body.daily.data[0].temperatureHigh +' degrees and the low is '+ body.daily.data[0].temperatureLow +  " degrees."
             )
         }
     })
